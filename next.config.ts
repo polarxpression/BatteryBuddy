95090b4a843823793800ccd12986400c5f3dc0c6
polarfreez <rafaelbraun5@hotmail.com>
2025-08-05 12:22:06 -0300

    Update next.config.ts to use @ducanh2912/next-pwa and add PWA configuration

--- next.config.ts
+++ next.config.ts
@@ -1,23 +1,20 @@
-import type {NextConfig} from 'next';
+/** @type {import('next').NextConfig} */
 
-const nextConfig: NextConfig = {
-  /* config options here */
-  typescript: {
-    ignoreBuildErrors: true,
+const withPWA = require('@ducanh2912/next-pwa').default({
+  dest: 'public',
+  cacheOnFrontEndNav: true,
+  aggressiveFrontEndNavCaching: true,
+  reloadOnOnline: true,
+  swcMinify: true,
+  disable: process.env.NODE_ENV === 'development',
+  workboxOptions: {
+    disableDevLogs: true,
   },
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
-  images: {
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'placehold.co',
-        port: '',
-        pathname: '/**',
-      },
-    ],
-  },
+});
+
+const nextConfig = {
+  output: 'export',
+  basePath: process.env.PAGES_BASE_PATH,
 };
 
-export default nextConfig;
+module.exports = withPWA(nextConfig);
