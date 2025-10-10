b9f67fed613b2b0fc5f9e62026a7605a421f8fd0
polarfreez <rafaelbraun5@hotmail.com>
2025-10-10 15:10:26 -0300

    fix(image-proxy): stream response and add user-agent

--- src/app/api/image-proxy/route.ts
+++ src/app/api/image-proxy/route.ts
@@ -9,18 +9,32 @@ export async function GET(req: NextRequest) {
   }
 
   try {
-    const response = await fetch(imageUrl, { credentials: 'omit' });
+    const response = await fetch(imageUrl, {
+      headers: {
+        'User-Agent':
+          'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36',
+      },
+      credentials: 'omit',
+    });
     if (!response.ok) {
-      return new NextResponse('Failed to fetch image', { status: response.status });
+      return new NextResponse(
+        `Failed to fetch image. Status: ${response.status} ${response.statusText}`,
+        { status: response.status }
+      );
     }
-    const imageBlob = await response.blob();
-    return new NextResponse(imageBlob, {
+    const stream = response.body as ReadableStream<Uint8Array>;
+
+    return new NextResponse(stream, {
       headers: {
         'Content-Type': response.headers.get('Content-Type') || 'image/jpeg',
       },
     });
-  } catch (error) {
-    console.error('Error fetching image:', error);
+  } catch (error: unknown) {
+    if (error instanceof Error) {
+      console.error('Error fetching image:', error.message);
+    } else {
+      console.error('An unknown error occurred:', error);
+    }
     return new NextResponse('Internal Server Error', { status: 500 });
   }
 }
