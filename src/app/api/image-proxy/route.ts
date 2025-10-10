912c41bcaea6989cb1e306e9a7cdec076ddfb43e
polarfreez <rafaelbraun5@hotmail.com>
2025-10-10 16:26:42 -0300

    fix(image-proxy): add referer header to fetch request

--- src/app/api/image-proxy/route.ts
+++ src/app/api/image-proxy/route.ts
@@ -13,6 +13,7 @@ export async function GET(req: NextRequest) {
       headers: {
         'User-Agent':
           'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36',
+        'Referer': 'https://www.panasonic.com/',
       },
       credentials: 'omit',
     });
